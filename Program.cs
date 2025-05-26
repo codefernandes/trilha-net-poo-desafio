a0ca5f49899fc6a00c07647a2f7df825b3b83039
Marcelo Fernandes <codefernandes@gmail.com>
2025-05-26 02:48:17 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+	Nokia nokia = new Nokia("111111111", "Nokia A90", "8987676767666556", 16);
+	
+	nokia.Ligar();
+	nokia.ReceberLigacao();
+	nokia.InstalarAplicativo("Photoshop");
+
+	Iphone iphone = new Iphone("2222222", "Nokia A90", "898766564544766556", 16);
+
+	iphone.Ligar();
+	iphone.ReceberLigacao();
+	iphone.InstalarAplicativo("Photoshop");
